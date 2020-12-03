b23cf7c8ff7f1987253fdb1fb321a0e695b1d327
qaz852tgb963 <65478824+qaz852tgb963@users.noreply.github.com>
2020-12-03 21:31:26 +0800

    VS

---
+++ 2DGame/Assets/Scripts/Car.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    string name { get; set; }
+    float size { get; set; }
+
+    new Car(string name,float size)
+    {
+        name = name,
+        size = size
+    }
+}
