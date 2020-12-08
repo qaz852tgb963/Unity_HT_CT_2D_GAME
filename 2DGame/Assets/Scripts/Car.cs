bf22d825f44d80196b4b5596753b17efcd34e861
qaz852tgb963 <65478824+qaz852tgb963@users.noreply.github.com>
2020-12-08 21:29:24 +0800

    程式練習

--- 2DGame/Assets/Scripts/Car.cs
+++ 2DGame/Assets/Scripts/Car.cs
@@ -2,12 +2,28 @@
 
 public class Car : MonoBehaviour
 {
-    string name { get; set; }
-    float size { get; set; }
+    public int iSize;
 
-    new Car(string name,float size)
+    [Header("擁有"), Tooltip("是否擁有"),Range(float.MinValue, float.MaxValue)]
+    public float iFloat = float.MinValue;
+
+    public string sName;
+
+    [Header("擁有"),Tooltip("是否擁有")]
+    public bool bHave;
+
+    public Color colorA = new Color(255, 255, 255, 0.5f);
+
+    public Vector2 v2A = Vector2.one;
+    public Vector2 v2B = Vector2.zero;
+
+    public AudioClip AC1;
+    public Sprite imgA;
+
+    private void Start()
     {
-        name = name,
-        size = size
+        print(colorA);
     }
+
+    
 }
---
+++ 2DGame/Assets/Scripts/TetrisManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TetrisManager : MonoBehaviour
+{
+    [Header("掉落時間"), Range(0.1f, 3)]
+    public float iSpeed = 1.5f;
+
+    [Header("目前分數")]
+    public int iNowScore;
+
+    [Header("最高分數")]
+    public int iHighestScore;
+
+    [Header("等級"), Range(1, 999)]
+    public int iLevel = 1;
+
+    [Header("結束畫面")]
+    public GameObject gGameOver;
+
+    [Header("音效：掉落、移動、清除、結束")]
+    public AudioClip aDownAudio;
+    public AudioClip aLeftRightAudio;
+    public AudioClip aClearAudio;
+    public AudioClip aGameOverAudio;
+
+
+
+}
