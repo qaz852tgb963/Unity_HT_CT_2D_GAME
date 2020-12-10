2a4ec5a8a6e60026037541038518e15464904557
qaz852tgb963 <65478824+qaz852tgb963@users.noreply.github.com>
2020-12-10 21:14:07 +0800

    程式更新

---
+++ 2DGame/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+    public void PlayGame()
+    {
+
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void LeaveGame()
+    {
+
+    }
+}
--- 2DGame/Assets/Scripts/TetrisManager.cs
+++ 2DGame/Assets/Scripts/TetrisManager.cs
@@ -2,6 +2,8 @@
 
 public class TetrisManager : MonoBehaviour
 {
+    #region 欄位
+
     [Header("掉落時間"), Range(0.1f, 3)]
     public float iSpeed = 1.5f;
 
@@ -23,6 +25,66 @@ public class TetrisManager : MonoBehaviour
     public AudioClip aClearAudio;
     public AudioClip aGameOverAudio;
 
+    #endregion
 
+    #region 事件
 
+    private void Start()
+    {
+
+    }
+
+    #endregion
+
+    #region 方法
+
+    /// <summary>
+    /// 生成俄羅斯方塊
+    /// </summary>
+    private void GenerateBlock()
+    {
+
+    }
+
+    /// <summary>
+    /// 添加分數
+    /// </summary>
+    /// <param name="iScore">增加的分數</param>
+    public void AddScore(int iScore)
+    {
+
+    }
+
+    /// <summary>
+    /// 遊戲時間
+    /// </summary>
+    private void GameTime()
+    {
+
+    }
+
+    /// <summary>
+    /// 遊戲結束
+    /// </summary>
+    private void GameOver()
+    {
+
+    }
+
+    /// <summary>
+    /// 重新遊戲
+    /// </summary>
+    public void ReplayGame()
+    {
+
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void LeaveGame()
+    {
+
+    }
+    #endregion
 }
